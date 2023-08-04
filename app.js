f51aca711bab98a560134b50df3c91344954ad4e
Jean LAURENT <jeanz.laurent@gmail.com>
2023-08-04 14:11:53 +0200

    edit progress display

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ function handlePlayPause() {
         toggleBtn.firstElementChild.src = './resources/play.svg';
         toggleBtn.firstElementChild.alt = 'Play icon';
         toggleBtn.setAttribute('data-toggle', 'play');
-        handleClassAnimation({work: false, rest: false});
+        handleClassAnimation({work: false, rest: false}, true)
     }
 }
 
@@ -67,7 +67,7 @@ function handleTicks() {
     } else if (!pause && !workTime && !restTime) {
         cycleCount++;
         cycles.textContent = `Cycles : ${cycleCount}`;
-        workTime = workTimeDefault;
+        workTime = workTimeDefault - 1;
         restTime = restTimeDefault;
         displayWork.textContent = formattedTime(workTime);
         displayRest.textContent = formattedTime(restTime);
@@ -75,32 +75,32 @@ function handleTicks() {
     }
 }
 
-function handleClassAnimation({work, rest}) {
-    let workDisplay = document.querySelector(`.work`);
-    let workProgressBar = workDisplay.parentNode.querySelector('.progress-bar');
-    let restDisplay = document.querySelector(`.rest`);
-    let restProgressBar = restDisplay.parentNode.querySelector('.progress-bar');
-    if (work) {
-        document.querySelector('.work').classList.add('active');
-        document.querySelector('.rest').classList.remove('active');
-        let width = 100 - (workTime / workTimeDefault * 100);
-        workProgressBar.style.width = `${width}%`;
-        workProgressBar.style.transition = 'width 1s linear';
-    } else {
-        document.querySelector('.work').classList.remove('active');
-        workProgressBar.style.width = '0';
-        workProgressBar.style.transition = 'none';
+function handleClassAnimation({work, rest}, pause = false) {
+    let itemState = {work, rest};
+    for (const item in itemState) {
+        if (itemState[item]) {
+            document.querySelector(`.${item}`).classList.add('active');
+        } else {
+            document.querySelector(`.${item}`).classList.remove('active');
+        }
     }
-    if (rest) {
-        document.querySelector('.rest').classList.add('active');
-        document.querySelector('.work').classList.remove('active');
-        let width = 100 - (restTime / restTimeDefault * 100);
-        restProgressBar.style.width = `${width}%`;
-        restProgressBar.style.transition = 'width 1s linear';
+    if (!pause) {
+        let workProgressBar = document.querySelector('.work ~ .progress-bar');
+        let restProgressBar = document.querySelector('.rest ~ .progress-bar');
+
+        displayProgress(work, workTime, workTimeDefault, workProgressBar);
+        displayProgress(rest, restTime, restTimeDefault, restProgressBar);
+    }
+}
+
+function displayProgress(item, time, defaultTime, progressBar) {
+    if (item) {
+        let width = 100 - (time / defaultTime * 100);
+        progressBar.style.width = `${width}%`;
+        progressBar.style.transition = 'width 1s linear';
     } else {
-        document.querySelector('.rest').classList.remove('active');
-        restProgressBar.style.width = '0';
-        restProgressBar.style.transition = 'none';
+        progressBar.style.width = '0';
+        progressBar.style.transition = 'none';
     }
 }
 
